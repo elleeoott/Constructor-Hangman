9927919d5370e158d7d0f1a6505aa55f18e983a4
Evan Darst <evan.l.darst@gmail.com>
2018-01-10 21:17:07 -0500

    pseudocode for main.js

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ var inquirer = require("inquirer");
 var app = require("./app.js");
 var words = require("./words.js");
 
+//variable holds our game initialization
 var gameHangman = {
+
+    //array holds the letters guessed by the user
+
+    //check to see if user guessed the word
 
 
 }
+
+//prompts for the user the play the game
+
+//if confirmed, starts a new game
 
 gameHangman();
