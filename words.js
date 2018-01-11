2d5ae06a75094e03f759b573d0367a178dfdabcb
Evan Darst <evan.l.darst@gmail.com>
2018-01-10 20:43:51 -0500

    created function for holding letters array

---
+++ words.js
@@ -0,0 +1,13 @@
+//require our letters.js object
+
+var gameLetters = require("./letters.js");
+
+function gameWords(word) {
+
+    //this stores the empty array of words
+    
+    this.gameWords = word;
+    this.letters = []; 
+}
+
+module.exports = words;
