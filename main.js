fc75818824934555f4668b77612127f2e039d008
Evan Darst <evan.l.darst@gmail.com>
2018-01-11 07:12:17 -0500

    dded empty array for game start

--- main.js
+++ main.js
@@ -3,18 +3,36 @@
 var inquirer = require("inquirer");
 var app = require("./app.js");
 var words = require("./words.js");
+var isLetter = require("is-letter");
 
 //variable holds our game initialization
 var gameHangman = {
 
     //array holds the letters guessed by the user
 
+    guessesLeft: 10;
+    guessedLetters: [];
+    gameStart: function() {
+
+        //makes sure that guessedLetters is cleared before a new game
+
+        if(this.guessedLetters.length > 0) {
+
+            this.guessedLetters = [];
+        }
+
+    }
+
+    //prompts for the user the play the game
+
+    inquirer.prompt
+
     //check to see if user guessed the word
 
 
 }
 
-//prompts for the user the play the game
+
 
 //if confirmed, starts a new game
 
